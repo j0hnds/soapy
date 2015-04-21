b0a92e8bd363ed982f6be7d14374660a9fc611b8
Phillip Doise <p.doise@gmail.com>
2015-04-21 14:33:06 -0600

    commiting while it still works

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   wash_out :matrix_cares
+  wash_out :origami_risks
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
